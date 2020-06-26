d3ca80dbe64b9cf89ae585502555ea2aee52cd1c
andrewstephens99 <andrewstephens.github@gmail.com>
2020-06-26 11:52:21 -0500

    die class fully functional and tested

---
+++ DnDDiceRollerCore/Die.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DnDDiceRollerCore
+{
+	public class Die
+	{
+		public string name { get; set; }
+		public int min { get; set; }
+		public int max { get; set; }
+
+		public Die(string name, int min, int max)
+		{
+			this.name = name;
+			this.min = min;
+			this.max = max;
+		}
+
+		public int roll()
+		{
+			Random r = new Random();
+			Console.Write(r.Next(this.min, this.max));
+			return r.Next(this.min, this.max);
+		}
+	}
+}
+
